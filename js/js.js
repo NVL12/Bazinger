8fc327e15483f9613868cc8ef44edd3c029af547
NVL12 <43294269+NVL12@users.noreply.github.com>
2019-02-24 22:22:06 +0700

    add js

---
+++ js/js.js
@@ -0,0 +1,18 @@
+var slideIndex = 1;
+showSlide(slideIndex);
+function plusSlide(n) {
+    showSlide(slideIndex += n);
+}
+function showSlide(n) {
+    var slides = document.getElementsByClassName("slide-content");
+    var dots = document.getElementsByClassName("btn-dot-slide");
+    if (n > slides.length) 
+        slideIndex = 1;
+    var i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].style.backgroundColor = "rgba(255,255,255,0)";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].style.backgroundColor = "blue";
+}
